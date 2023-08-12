31ebea3bbb53a4617f701cd7b9b2db6f359ba899
fbriones92 <primer1grupo@gmail.com>
2023-08-12 15:31:40 -0500

    pruebas 2

--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class Usuario(AbstractUser):
-    # Agrega campos personalizado
+    # Agrega campos personalizados
     fecha_nacimiento = models.DateField(null=True, blank=True)
     rol_id = models.IntegerField(blank=True)
     
